a01ca7b57d9e9ba531d63769686bffff116b3978
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2020-05-27 21:56:57 +0200

    Use clangstdenv instead of stdenv

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 
-pkgsi686Linux.stdenv.mkDerivation {
+pkgsi686Linux.clangStdenv.mkDerivation {
   pname = "warzone";
   version = "0.1";
   preConfigure= "./autogen.sh";
@@ -20,10 +20,11 @@ pkgsi686Linux.stdenv.mkDerivation {
 
   hardeningDisable = [ "all" ];
 
-  # Wow, that did the trick! :)
   LDFLAGS = "-lX11 -L" + pkgsi686Linux.llvmPackages.clang-unwrapped + "/lib/";
+  #  + "-I" + pkgsi686Linux.glibc.dev + "/";
   LIBCLANG_PATH = pkgsi686Linux.llvmPackages.libclang + "/lib/";
-  LIBRARY_PATH = pkgsi686Linux.llvmPackages.clang-unwrapped + "/lib/";
+  #LIBRARY_PATH = pkgsi686Linux.glibc + "/lib/";
+  #CLANG_LIBDIR_SUFFIX = pkgsi686Linux.glibc.dev + "/";
 
   #configureFlags = [ "--libs" "x11" "xrandr" "xi" "xxf86vm" "glew" "glfw3"];
   #buildFlags = [ "-lX11" ];
